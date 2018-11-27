674888f33831e8cb9befce23433ebf362db5e8e1
nhonduyen <nhonduyen@gmail.com>
2018-11-27 10:36:24 +0700

    First commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(candidate); i++)
+            {
+                if (candidate % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
